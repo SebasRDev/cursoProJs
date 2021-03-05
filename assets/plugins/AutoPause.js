39e700d51842e633b21f498efdd3ad91476270cc
Sebastian Ramirez <sebasramirez.dev@gmail.com>
2021-03-05 16:51:58 -0500

    intersection observer class and visibility change

--- assets/plugins/AutoPause.js
+++ assets/plugins/AutoPause.js
@@ -1,17 +1,20 @@
 class AutoPause {
-  constructor() {
+  constructor(){
     this.threshold = 0.25;
     this.handleIntersection = this.handleIntersection.bind(this);
+    this.handleVisbilityChange = this.handleVisbilityChange.bind(this);
   }
 
-  run(player) {
+  run(player){
     this.player = player;
 
     const observer = new IntersectionObserver(this.handleIntersection, {
-      threshold: this.threshold,
+      threshold: this.threshold
     });
 
     observer.observe(this.player.media);
+
+    document.addEventListener("visibilitychange", this.handleVisbilityChange);
   }
 
   handleIntersection(entries) {
@@ -19,9 +22,19 @@ class AutoPause {
 
     const isVisible = entry.intersectionRatio >= this.threshold;
 
-    if (isVisible) {
+    if(isVisible){
       this.player.play();
-    } else {
+    }else{
+      this.player.pause();
+    }
+  }
+
+  handleVisbilityChange() {
+    const isVisible = document.visibilityState === "visible"
+
+    if( isVisible ){
+      this.player.play()
+    }else{
       this.player.pause();
     }
   }
